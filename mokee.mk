4ea9c9658d6a53827986b4f62bb9d11c9e0e9555
martincz <martincz@mokeedev.com>
2017-01-19 14:50:13 +0800

    kltechnduo: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/kltechnduo/full_kltechnduo.mk)
 
 PRODUCT_DEVICE := kltechnduo
-PRODUCT_NAME := lineage_kltechnduo
+PRODUCT_NAME := mk_kltechnduo
